b3d3acac41e61f5cc704433aa7384d47e69c3653
青戈不再 <375345404@qq.com>
2021-11-21 17:28:15 +0800

    first commit

---
+++ src/views/prologue/prologue.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<!--        <div>十年如一梦</div>-->
+<!--        <div>沧海旷浮生</div>-->
+<!--        <div>不知海棠处</div>-->
+<!--        <div>风雪二三更</div>-->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "prologue"
+    }
+</script>
+
+<style scoped>
+
+</style>
